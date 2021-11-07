669773775df4d48795437a640f69546ea5e00dba
Mathias Berger--fruhinsholz <mberger-@c2r2p6.42nice.fr>
2021-11-07 15:49:01 +0100

    clean up

--- test.sh
+++
@@ -1,7 +0,0 @@
-for i in {1..100}
-do
-	VALUES=`ruby -e "puts (0..500).to_a.shuffle.join(' ')"`
-	N=./push_swap $VALUES | wc -l
-	VALID=./push_swap $VALUES | ./checker $VALUES
-	echo $VALID $N
-done
